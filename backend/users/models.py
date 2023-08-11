95faae457174125539109edc8550cea92719a8dd
Vladislav Pipkin <pipkin.vladislav@yandex.ru>
2023-08-11 18:52:16 +0300

    Added base user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(verbose_name='Электронная почта', max_length=254)
+    first_name = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=150)
+    last_name = models.CharField(
+        verbose_name='Фамилия пользователя',
+        max_length=150)
