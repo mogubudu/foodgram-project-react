65bf7fe7f83885de93da7b933b5ccfa9d857dde2
Vladislav Pipkin <pipkin.vladislav@yandex.ru>
2023-08-11 22:13:28 +0300

    Delete secret key in setting and create new

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&q9yo=0#tudvp6tlza$!*))10u391qws9#_rry6kj$mt(uh^_%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') in ('True',)
 
 ALLOWED_HOSTS = []
 
