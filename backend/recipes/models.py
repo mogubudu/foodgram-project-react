837d24a6a9baf2d513b38738b1975199403dd33e
Vladislav Pipkin <pipkin.vladislav@yandex.ru>
2023-08-11 19:38:42 +0300

    Added base models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name='Название ингредиента',
+        max_length=200
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Единица измерения',
+        max_length=200
+    )
+
+
+class IngredientAmount(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField('Количество ингредиента')
+
+
+class Tag(models.Model):
+    name = models.CharField(verbose_name='Название тега', max_length=200)
+    color = models.CharField(verbose_name='Цветовой HEX-код', max_length=7)
+    slug = models.SlugField(verbose_name='Идентификатор URL')
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    name = models.CharField(verbose_name='Название рецепта', max_length=200)
+    image = models.ImageField(verbose_name='Изображение рецепта')
+    text = models.TextField(verbose_name='Текстовое описание')
+    ingredients = models.ManyToManyField(Ingredient, through=IngredientAmount)
+    tags = models.ManyToManyField(Tag)
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления в минутах'
+    )
+
+    class Meta:
+        default_related_name = 'recipes'
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
